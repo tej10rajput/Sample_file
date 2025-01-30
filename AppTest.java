a8258a8a17c539b8b78698ae5016ca6b399691b4
tej10rajput <127550618+tej10rajput@users.noreply.github.com>
2025-01-30 12:19:05 +0530

    3 files uploaded

---
+++ AppTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
